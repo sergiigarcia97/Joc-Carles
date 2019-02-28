e5a7d7cd350a668321e8df2d365bf4229ea78842
Sergi <sergiigarcia97@gmail.com>
2019-02-28 12:40:12 +0100

    Modificació general
    1- borrat bootstrap 4.2
    2- arreglat estructura carpetes/fitxers
    3- intent control audio (comentat)
    4- estil v2 arreglat
    5- estil enemicv2 arreglat i modificat (posicio text)
    6- afegit jquery
    7- canviat javascript per jquery
    8- estructura tabulacions

---
+++ js/funcions.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+      //haig de repassaro, de moment ho deixem
+      /*
+      $(".controlAudio").click(function(){
+          var src = $(this).attr('src');
+
+          // si no es fa el else, no es podria tornar a encender
+          var canviarsrc;
+          var controlAudio = document.getElementById("controlAudio");
+          if(src=='img/speakon.png'){
+              canviarsrc = 'img/speakoff.png';
+              $('#controlAudio').trigger("play");
+          }else{
+              canviarsrc = 'img/speakon.png';
+              $('#controlAudio').trigger("pause");
+          }
+          $(this).attr('src', canviarsrc );
+      });
+      */
+}
--- js/gamemanager.js
+++ js/gamemanager.js
@@ -19,43 +19,48 @@ let GameManager = {
                 player = new Player(classType, 200, 0, 50, 200, 100);
                 break;
         }
-        //Aixó ens agafa el primer interface que troba dintre del codi ho fa a partir del querySelector
-        let getInterface = document.querySelector(".interface");
-        //Aquesta linia ens serveix per escriure els stats a la pagina, realment és HTML que hem ficat amb el innerHTML
-        getInterface.innerHTML ='<div class="Characterv2">'
-            +'<img src="img\\' + classType.toLowerCase() + '.jpg" alt="'+classType+'" class="imgCharacterv2">'
-            +'<div class="textCharacterv2">'
-                +'<h3 class="titolCharacterv2">'+classType+'</h3>'
-                +'<div class="attrCharacterv2">'
-                    +'<p>Health: ' + player.health + '</p>'
-                    +'<p>Mana: ' + player.mana + '</p>'
-                    +'<p>Strength: ' + player.strength + '</p>'
-                    +'<p>Agility: ' + player.agility + '</p>'
-                    +'<p>Speed: ' + player.speed + '</p>'
+        //Aixó ens agafa el primer interface que troba dintre del codi ho fa a partir del querySelector / $
+        let getInterface = $(".interface");
+        //Aquesta linia ens serveix per escriure els stats a la pagina, realment és HTML que hem ficat amb el innerHTML / html()
+        getInterface.html(
+            '<div class="Characterv2">'
+                +'<img src="img\\' + classType.toLowerCase() + '.jpg" alt="'+classType+'" class="imgCharacterv2">'
+                +'<div class="textCharacterv2">'
+                    +'<h3 class="titolCharacterv2">'+classType+'</h3>'
+                    +'<div class="attrCharacterv2">'
+                        +'<p>Health: ' + player.health + '</p>'
+                        +'<p>Mana: ' + player.mana + '</p>'
+                        +'<p>Strength: ' + player.strength + '</p>'
+                        +'<p>Agility: ' + player.agility + '</p>'
+                        +'<p>Speed: ' + player.speed + '</p>'
+                        //Aqui creem un boto dintre del div actions, aquest boto servirá per començar el joc.
+                        + '<button class="btn-prefight" onclick="GameManager.setFight()">Enter the dungeon</button>'
+                    +'</div>'
                 +'</div>'
             +'</div>'
-        +'</div>';
+        );
+        $('.Characterv2').css('cursor','default');
     },
     setPreFight: function() {
       //Aqui creem 3 variables per després canviar el header, les actions i la arena , que són div's que hem creat dintre del HTML
-      let getHeader = document.querySelector(".header");
-      let getActions = document.querySelector(".actions");
-      let getArena = document.querySelector(".arena");
+      let getHeader = $(".header");
+      let getActions = $(".actions");
+      let getArena = $(".arena");
       //Aqui canviem el text del header
-      getHeader.innerHTML = '<h2 class="taskHeader">TASK: KILL THE BOSS!</h2>';
-      //Aqui creem un boto dintre del div actions, aquest boto servirá per començar el joc.
-      getActions.innerHTML = '<a class="btn-prefight" onclick="GameManager.setFight()">Enter the dungeon</a>';
-      getArena.style.visibility = "visible";
+      getHeader.html('<h2 class="taskHeader">TASK: KILL THE BOSS!</h2>');
+
+
+      //getArena.style.visibility = "visible";
     },
     setFight: function() {
-      let getHeader = document.querySelector(".header");
-      let getActions = document.querySelector(".actions");
-      let getEnemy = document.querySelector(".enemy");
-      // Crea un enemic aleatori
-      let enemy00 = new Enemy("orc", 100, 0, 50, 100, 100);
+      let getHeader = $(".header");
+      let getActions = $(".actions");
+      let getEnemy = $(".interfaceEnemic");
+      let enemy00 = new Enemy("Orc", 100, 0, 50, 100, 100);
       let enemy01 = new Enemy("Orc", 200, 0, 120, 50, 80);
+      // Crea un enemic aleatori
       var chooseRandomEnemy = (Math.floor(Math.random() * (2 - 0)) + 0);
-      console.log(chooseRandomEnemy);
+      //console.log(chooseRandomEnemy);
         switch(chooseRandomEnemy) {
             case 0:
                 enemy = enemy00;
@@ -64,9 +69,23 @@ let GameManager = {
                 enemy = enemy01;
                 break;
         }
-        document.getElementById('enemic').innerHTML = '<img src="img/' + enemy.enemyType.toLowerCase() + '.jpg" class="imgEnemy" alt="' + enemy.enemyType + '"class="img-avatar"><div><h3 class="titolEnemy">' + enemy.enemyType + '</h3><div class="textEnemy"<p class="health-enemy">Health: ' + enemy.health +'</p><p>Mana: ' + enemy.mana + '</p><p>Strength: ' + enemy.strength + '</p><p>Agility: ' + enemy.agility + '</p><p>Speed: ' + enemy.speed + '</p></div>';
-        getHeader.innerHTML = 'WHAT YOU WANNA DO?';
-        getActions.innerHTML = '<a class="btn-fight" onclick="PlayerMoves.calcAttack()">ATTACK!</a>';
-        
+        getEnemy.html(
+            '<div class="Enemyv2">'
+                + '<img src=img/' + enemy.enemyType.toLowerCase() + '.jpg class="imgEnemy" alt=' + enemy.enemyType + '>'
+                + '<div class="textEnemy">'
+                    + '<h3 class="titolEnemy">' + enemy.enemyType + '</h3>'
+                    + '<div class="attrEnemyv2"'
+                        + '<p>Health: ' + enemy.health +'</p>'
+                        + '<p>Mana: ' + enemy.mana + '</p>'
+                        + '<p>Strength: ' + enemy.strength + '</p>'
+                        + '<p>Agility: ' + enemy.agility + '</p>'
+                        + '<p>Speed: ' + enemy.speed + '</p>'
+                    + '</div>'
+                + '</div>'
+            + '</div>'
+        );
+        getHeader.html( '<h2 class="headerText">WHAT YOU WANNA DO?</h2>');
+        //getActions.html( '<a class="btn-fight" onclick="PlayerMoves.calcAttack()">ATTACK!</a>');
+        getEnemy.show();
     }
 }
